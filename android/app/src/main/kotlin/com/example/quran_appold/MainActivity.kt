d862ae23e8d7c24e3157f7cd6f01e9027e47ace8
Win11-Pro <mohammedalghoul@gmail.com>
2022-04-03 16:04:59 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/quran_appold/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quran_appold
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_settings.dart
@@ -0,0 +1,7 @@
+class AppSettings {
+  // TODO: Add key and iv to enable download translations
+  static String key = '';
+  static String iv = '';
+  
+  static Map<String, dynamic> secrets;
+}
